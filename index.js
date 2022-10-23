13de4f07fca925e956fec8c7ebd25aed3c2c6f04
AmmarEldik <vektorea60@gmail.com>
2022-10-23 15:42:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const get = document.querySelector("#main");
+get.parentNode.removeChild(get);
+
+
+
+const newHeader = document.createElement("h1");
+const nodeText = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(nodeText);
+newHeader.setAttribute("id","victory");
+
